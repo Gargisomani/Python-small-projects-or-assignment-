3b0a6e87bb66431626bf6b0ee0c9778aeb0a8949
Gargisomani <43922479+Gargisomani@users.noreply.github.com>
2025-08-06 17:19:01 +0530

    Add files via upload

---
+++ Module3_Task 1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number: "))
+def factorial(num):
+    if num==1:
+        return 1
+    else:
+        return num*factorial(num-1)
+result=factorial(num)
+print("Factorial of",num,"is:",result)
